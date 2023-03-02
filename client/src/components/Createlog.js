99d5f5ead4a0fd8a2615e25b352756dfe9938f02
Balkrishna pokharel <58511308+krishbale@users.noreply.github.com>
2023-03-02 18:05:55 +0000

    createlogdesigned

--- client/src/components/Createlog.js
+++ client/src/components/Createlog.js
@@ -1,6 +1,19 @@
 
 import React,{useState,useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
+
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+// import signpic from "../images/signup.svg";
+import {  useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 export default function Createlog() {
   const history = useNavigate();
@@ -36,7 +49,7 @@ export default function Createlog() {
 }
   const handleInputs = (e) => {
     let name, value;
-    console.log(e)
+    // console.log(e)
     name = e.target.name;
     value = e.target.value;
     setUser({ ...user,[name]:value})
@@ -45,37 +58,94 @@ export default function Createlog() {
 
   return (
     <>
-      <div className='createlog'>
-                <h1>Createlog</h1>
-                <div className='row'>
-                <form className='form ' method='post' >
-                <div className='form-group '>
-                
-
-                    <label  htmlFor="title"> Enter the title for your travel log: </label>
-                    <input type="text" className='form-control form-text text bg-dark  text-white ' placeholder='title' name="title"           
-                        onChange={handleInputs} required/>
-
-                    <label htmlFor="descriptions">Explain your experience about your exprediton: </label>
-                   <textarea className='form-control form-control-plaintext bg-dark  text-white ' placeholder='descriptions' name="descriptions" 
-                    onChange={handleInputs} id="" cols="50" rows="10"></textarea>
-
-                    <label htmlFor="days"> Days you spent on your travel: </label>
-                    <input type="number" className='form-control bg-dark  text-white text-white ' placeholder='days' name="days"
-                        onChange={handleInputs} required/>
-
-                    <label htmlFor="budgets">Enter the budget for your travel</label>
-                  
-                    <input type="number" className='form-control bg-dark  text-white '  placeholder='budgets' name="budgets"
-                    onChange={handleInputs} required/>
-                    <input type="submit" className='btn btn-lg btn-outline-info '  name='submit' onClick={Travelform} />
-                    </div>
-                  
-                </form>
-
-                </div>
-               
-            </div>
+     <Container component="main" maxWidth="xs">
+     <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+      
+          <Typography component="h1" variant="h5">
+          Create a Log
+          </Typography>
+          <Box component="form" onSubmit={Travelform} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              type="text"
+              id="title"
+              label="Enter the title for your travel log:"
+              name="title"
+              autoComplete=""
+              autoFocus
+              onChange={handleInputs} 
+            />
+              <TextField
+              margin="normal"
+              required
+              fullWidth
+              type="text"
+              id="descriptions"
+              label="Explain your experience about your exprediton:"
+              name="descriptions"
+              autoComplete="descriptions"
+              autoFocus
+              onChange={handleInputs} 
+            />
+              <TextField
+              margin="normal"
+              required
+              fullWidth
+              type="number"
+              id="days"
+              label=" Days you spent on your travel:"
+              name="days"
+              autoComplete="days"
+              autoFocus
+              onChange={handleInputs} 
+            />
+              <TextField
+              margin="normal"
+              required
+              fullWidth
+              type="number"
+              id="budget"
+              label="Enter the budget for your travel"
+              name="budgets"
+              autoComplete="budgets"
+              autoFocus
+              onChange={handleInputs} 
+            />
+           
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+             Create New LOG
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="/" variant="body2">
+                  Back to home ?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/view" variant="body2">
+                  {"Explore the log? "}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        </Container>
+      
        
     </>
   )
