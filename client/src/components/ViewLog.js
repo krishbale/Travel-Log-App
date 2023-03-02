ac708f3ee05372c6b6cea4f473cf841736632700
balkrishna pokharel <sushil.pokharel.7528@gmail.com>
2023-03-03 01:37:53 +0545

    listcardforlog

--- client/src/components/ViewLog.js
+++ client/src/components/ViewLog.js
@@ -1,10 +1,26 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
-
-
-
-
+// import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+// import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+// import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+// import Stack from '@mui/material/Stack';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+// import Link from '@mui/material/Link';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+import CommentIcon from '@mui/icons-material/Comment';
 export default function ViewLog() {
+    
+    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     
     const [userData, setUserData] = useState([])
     const [comments,setComments] = useState('')
@@ -98,8 +114,56 @@ export default function ViewLog() {
 
     return (
         <>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {userData.map((item) => (
+              <Grid item key={item} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    {item.title}
+                    </Typography>
+                    <Typography>
+                    {item.descriptions} 
+                    </Typography>
+                    <Typography>
+                    Days:{item.days} 
+                    </Typography>
+                    <Typography>
+                    Budgets:{item.budgets} 
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button onClick={()=>{postlike(item._id)}} size="small">
+                    {}
+                    {likes} <FavoriteIcon />
+                    </Button>
+                    <Button size="small">
+                        <CommentIcon />
+                    </Button>
+                    <Button size="small">
+                       <QuestionAnswerIcon />
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
         
-        <h1 >ViewLog</h1>
+{/*         
         <div className='row'>
        
     
@@ -179,7 +243,7 @@ export default function ViewLog() {
 
 
 
-             </div>
+             </div> */}
         </>
     )
 
