6538dd4d5562fcee86a05e1f48917cd86b58343e
Balkrishna pokharel <58511308+krishbale@users.noreply.github.com>
2023-03-02 15:30:44 +0000

    loginformadded

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,20 @@
 import React,{useContext, useState} from 'react'
 import loginpic from '../images/login.svg'
-import { Link,useNavigate  } from 'react-router-dom';
+import { useNavigate  } from 'react-router-dom';
 import {  UserContext } from '../App';
-
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+// import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
 const LoginLayout = () => {
 const {state,dispatch} = useContext(UserContext);
   const history = useNavigate()
@@ -52,8 +64,74 @@ const {state,dispatch} = useContext(UserContext);
 
   return (
     <>
+      <Container component="main" maxWidth="xs">
+     <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={loginUser} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              type="text"
+              id="username"
+              label="User Name"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        </Container>
    
-      <section className='sign-in bg-dark'>
+      {/* <section className='sign-in bg-dark'>
       <div className='container mt-5'>
       <div className='signin-content'>
       <div className='signin-image bg-dark'>
@@ -116,7 +194,7 @@ const {state,dispatch} = useContext(UserContext);
      
 
 
-    </section>
+    </section> */}
     </>
   )
 }
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,11 +1,21 @@
 import React, {useState} from 'react'
-import signpic from "../images/signup.svg";
-import { Link, useNavigate } from 'react-router-dom';
-
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+// import signpic from "../images/signup.svg";
+import {  useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 function SignupLayout  ()  {
-  const writer = "writer"
-  const viewer ="viewer"
+  // const writer = "writer"
+  // const viewer ="viewer"
   const   history = useNavigate();
   const [user,setUser] = useState({
     username:"",password:"",roles:"writer"
@@ -68,7 +78,73 @@ function SignupLayout  ()  {
 
   return (
    <>
-    <section className='signup bg-dark'>
+       <Container component="main" maxWidth="xs">
+     <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={PostData} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              type="text"
+              id="username"
+              label="User Name"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              onChange={handleInputs} 
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleInputs} 
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Already have an Account? Sign IN"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        </Container>
+    {/* <section className='signup bg-dark'>
     <div className='container mt-5 '>
     <div className='signup-content'>
     <div className='signup-form'>
@@ -124,7 +200,7 @@ function SignupLayout  ()  {
 
     </div>
 
-    </section>
+    </section> */}
    </>
   )
 }
